756b0a9c578ca8323c8170937e7c9dc43c040f90
Andrew Snowden <andrewsnowdenoit@gmail.com>
2023-04-06 17:58:59 -0700

    Updated directory structure and created the Cryptanalysis project

---
+++ Cryptanalysis/Cryptanalysis.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <fstream>  //Read and write files
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::fstream;
+using std::string;
+
+
+int main()
+{
+
+
+	return 0;
+}
